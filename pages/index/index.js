41b6ca6363b5d9e4fdc4b9ace742ff7bba984310
Jawsro <roro1990@163.com>
2021-01-18 09:12:53 +0800

    项目初建

---
+++ pages/index/index.js
@@ -0,0 +1,131 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 500,
+    indicatorDots: true,
+    indicatorActiveColor: "white",
+    grid:[
+      {
+        url:"icon-shuifeiguanli",
+        text:"水费"
+      },
+      {
+        url:"icon-dianfeifuwu",
+        text:"电费"
+      },
+      {
+        url:"icon-icon_tingchejiaofei-copy",
+        text:"停车费"
+      },
+      {
+        url:"icon-baoxiu",
+        text:"物业报修"
+      },
+      {
+        url:"icon-fuli",
+        text:"业主福利"
+      },
+      {
+        url:"icon-qiandao",
+        text:"签到积分"
+      },
+      {
+        url:"icon-fuwu",
+        text:"便民服务"
+      },
+      {
+        url:"icon-19",
+        text:"更多"
+      },
+    ],
+    classification:[
+      {
+        icon:"icon-wuyefeiyongchuzhang",
+        text:"物业缴费"
+      },
+      {
+        icon:"icon-gonggao",
+        text:"社区公告"
+      },
+      {
+        icon:"icon-wuyefeiyongchuzhang",
+        text:"智能开锁"
+      },
+      {
+        icon:"icon-fangkeshuxing",
+        text:"访客同行"
+      }
+    ]
+  },
+  goLogin(){
+    wx.navigateTo({
+      url: '../login/login',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,92 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    phoneLoginIsShow:false,
+    getCodeIsShow:false,
+    secondCode:60,
+    codeText:"获取验证码",
+    phoneLoginIsDisabled:true
+  },
+  // 事件处理函数
+  phoneLogin(){
+    this.setData({
+      phoneLoginIsShow:true
+    })
+  },
+  close(){
+    this.setData({
+      phoneLoginIsShow:false
+    })
+  },
+  getCode(){
+    var countdown = 60;
+    const _this = this;
+    var setTime=function(_this){
+      if(countdown==0){
+        _this.setData({
+          getCodeIsShow:false
+        })
+        countdown = 60;
+        return;
+      }else{
+        countdown--;
+        _this.setData({
+          getCodeIsShow:true,
+          secondCode:countdown
+        })
+        setTimeout(function () {
+          setTime(_this)
+        }, 1000)
+      }
+    }
+    setTime(_this);
+  },
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
